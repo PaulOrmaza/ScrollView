edf7c6e4e0a93adfaf827db80d5ce021563b778c
PaulOrmaza <steven.ormaza816@ist17dejulio.edu.ec>
2024-01-07 23:48:31 -0500

    Ejemplo acabado de ScrollView

--- app/src/main/java/com/spoj/ejemplo/MainActivity.java
+++ app/src/main/java/com/spoj/ejemplo/MainActivity.java
@@ -3,6 +3,7 @@ package com.spoj.ejemplo;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,43 +12,98 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn0 = findViewById(R.id.btn0);
+        Button btn1 = findViewById(R.id.btn1);
+        Button btn2 = findViewById(R.id.btn2);
+        Button btn3 = findViewById(R.id.btn3);
+        Button btn4 = findViewById(R.id.btn4);
+        Button btn5 = findViewById(R.id.btn5);
+        Button btn6 = findViewById(R.id.btn6);
+        Button btn7 = findViewById(R.id.btn7);
+        Button btn8 = findViewById(R.id.btn8);
+        Button btn9 = findViewById(R.id.btn9);
+        Button btn10 = findViewById(R.id.btn10);
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarMensaje("Diste click en 0");
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarMensaje("Diste click en 1");
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarMensaje("Diste click en 2");
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarMensaje("Diste click en 3");
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarMensaje("Diste click en 4");
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarMensaje("Diste click en 5");
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarMensaje("Diste click en 6");
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarMensaje("Diste click en 7");
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarMensaje("Diste click en 8");
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarMensaje("Diste click en 9");
+            }
+        });
+
+        btn10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarMensaje("Diste click en 10");
+            }
+        });
     }
 
-    public void clicBoton(View view) {
-        switch (view.getId()) {
-            case R.id.btn0:
-                Toast.makeText(this, "Diste click en 0", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.btn1:
-                Toast.makeText(this, "Diste click en 1", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.btn2:
-                Toast.makeText(this, "Diste click en 2", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.btn3:
-                Toast.makeText(this, "Diste click en 3", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.btn4:
-                Toast.makeText(this, "Diste click en 4", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.btn5:
-                Toast.makeText(this, "Diste click en 5", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.btn6:
-                Toast.makeText(this, "Diste click en 6", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.btn7:
-                Toast.makeText(this, "Diste click en 7", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.btn8:
-                Toast.makeText(this, "Diste click en 8", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.btn9:
-                Toast.makeText(this, "Diste click en 9", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.btn10:
-                Toast.makeText(this, "Diste click en 10", Toast.LENGTH_LONG).show();
-                break;
-        }
+    private void mostrarMensaje(String mensaje) {
+        Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show();
     }
 }
